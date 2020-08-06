6e444abdf85512fc0296dddef525ff01f60ab6f7
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-08-06 10:19:18 -0500

    transform data

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -7,7 +7,7 @@ const post = (props) => (
 		<h1>{props.title}</h1>
 		<h1>Title</h1>
 		<div className="Info">
-			<div className="Author">Author</div>
+			<div className="Author">{props.author}</div>
 		</div>
 	</article>
 );
