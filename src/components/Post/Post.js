9a9a3a2ecfbf56c51f0e70ca3e0726628da71c73
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-08-07 07:15:19 -0500

    make a post selectable

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Post.css';
 
 const post = (props) => (
-	<article className="Post">
+	<article className="Post" onClick={props.clicked}>
 		<h1>{props.title}</h1>
 		<h1>Title</h1>
 		<div className="Info">
