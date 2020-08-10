f084afe68b6baee1841bbbd97993e314f792bb87
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-08-10 08:45:37 -0500

    switch between pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import Posts from './Posts/Posts';
-import { Route } from 'react-router-dom';
+// import axios from 'axios';
+import { Route, Link } from 'react-router-dom';
 
 import './Blog.css';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
 	render() {
@@ -12,16 +14,26 @@ class Blog extends Component {
 					<nav>
 						<ul>
 							<li>
-								<a href="/">Home</a>
+								<Link to="/">Home</Link>
 							</li>
 							<li>
-								<a href="/new-post">New Post</a>
+								<Link
+									to={{
+										pathname: '/new-post',
+										hash: '#submit',
+										search: '?quick-submit=true'
+									}}
+								>
+									New Post
+								</Link>
 							</li>
 						</ul>
 					</nav>
 				</header>
-				{/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
+				{/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
 				<Route path="/" exact component={Posts} />
+				<Route path="/new-post" component={NewPost} />
 			</div>
 		);
 	}
