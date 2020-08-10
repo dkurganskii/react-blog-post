d7827c1bd25f0f72faa1b145398978dd09446142
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-08-10 09:45:34 -0500

    style the active route

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -5,7 +5,6 @@ import './Post.css';
 const post = (props) => (
 	<article className="Post" onClick={props.clicked}>
 		<h1>{props.title}</h1>
-		<h1>Title</h1>
 		<div className="Info">
 			<div className="Author">{props.author}</div>
 		</div>
